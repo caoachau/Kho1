e4dd951a5cb0d8f2f0e110e8ece4b048b1a50738
caoachau <147479885+caoachau@users.noreply.github.com>
2023-10-11 15:19:32 +0700

    Add files via upload

---
+++ nhapvaxuatmang.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	int n,i;
+	int a[100];
+	printf("Nhap so pt:"); 
+	scanf("%d", &n);
+for (int i =0;i <n; i++)
+  {
+    scanf("%d", &a[i]);
+   }
+    printf("Mang vua nhap la:");
+    for(i=0;i<n;i++)
+    {
+        printf("%d",a[i]);
+    }
+    
+}
+    
+    
+ 
